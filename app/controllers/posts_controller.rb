30d1b7aa02209fe9498623427a238be8a32e38fc
mikecoolio <mikepoon604@gmail.com>
2022-07-25 18:44:14 -0700

    created show/edit page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,28 @@
 class PostsController < ApplicationController
+    before_action :find_post_id, only: [:show, :update, :edit]
+
     def new
         @post = Post.new
     end
 
     def create
-        @post = Post.new(params.require(:post).permit(:title, :body))
+        @post = Post.new(post_params)
 
         if @post.save
             redirect_to post_path(@post), method: :post, notice: 'Post was successfully created.'
         else
+            flash[:error] = "Invalid post"
             render :new
         end
     end
 
     def show
-        @post = find_post_id
-
-        if @post.present? 
-            redirect_to(posts_path, :notice =>  'Post was successfully created.')
-        end
+        # if @post.present? 
+        #     redirect_to(posts_path, :notice =>  'Post was successfully created.')
+        # else
+        #     render :show
+        # end
+        @post = Post.find(params.require(:id))
     end
 
     def index
@@ -26,7 +30,9 @@ class PostsController < ApplicationController
     end
 
     def update
-        @post = find_post_id
+        if @post.update(post_params)
+            redirect_to posts_path, :notice => 'Post was successfully updated.'
+        end
     end
 
     def destroy
@@ -34,12 +40,15 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = find_post_id
     end
 
     private
 
     def find_post_id
-        Post.find(params[:id])
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
